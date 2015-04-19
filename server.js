828a7d118583adaab10819451669a78c0eca2ead
Max Goldstein <maxgoldstein1@gmail.com>
2015-04-19 15:09:52 -0400

    Server reports disconnect

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var io = require('socket.io')(8001);
 
 io.on('connection', function (socket) {
-  console.log("Client connected.")
+  console.log("Client connected.");
   socket.on('example', function (data) {
     console.log('Received:', data);
     socket.emit('example', data + 1);
+  });
+  socket.on('disconnect', function (socket) {
+    console.log("Client disconnected.");
   });
 });
